b9db46742013441da3f79ffd166f2c70679980ca
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-07-08 23:44:46 +0200

    Update dependencies and enhance PostCSS configuration

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,16 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  colors: {
+    primary: {
+      DEFAULT: "#45d8ac",
+      light: "#ffa94d",
+      dark: "#cc5c00",
+    },
+    border: "#e5e7eb",
+    success: "#22c55e",
+    warning: "#facc15",
+    danger: "#ef4444",
+  }
 };
 
 export default config;
