b310b915014fe3dfa6576dbc5b74e52a38b0c949
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-07-09 01:10:13 +0200

    Remove color configuration from PostCSS setup

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,16 +1,5 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
-  colors: {
-    primary: {
-      DEFAULT: "#45d8ac",
-      light: "#ffa94d",
-      dark: "#cc5c00",
-    },
-    border: "#e5e7eb",
-    success: "#22c55e",
-    warning: "#facc15",
-    danger: "#ef4444",
-  }
 };
 
 export default config;
