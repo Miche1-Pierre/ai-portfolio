2044f60867f1302c5e5fa3347f6c37cf2bf00070
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-07-09 18:36:44 +0200

    Add header comment for clarity in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,6 +49,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen flex flex-col justify-between">
+      {/* Header */}
       <header className="p-6 sm:p-10 flex justify-center">
         <NavigationMenu />
       </header>
