cfb3f76f017ebbc4e59c4ed80c1754533fb2ab66
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-07-09 20:03:06 +0200

    Add new badge colors and styles for various technologies and skills

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -14,7 +14,33 @@ export type BadgeColor =
   | "tailwind"
   | "node"
   | "php"
-  | "java";
+  | "java"
+  | "springboot"
+  | "express"
+  | "django"
+  | "MySQL"
+  | "Docker"
+  | "WampServer"
+  | "Trello"
+  | "python"
+  | "gitlab"
+  | "github"
+  | "postman"
+  | "virtualbox"
+  | "teams"
+  | "microsoft365"
+  | "curious"
+  | "leadership"
+  | "teamwork"
+  | "communication"
+  | "problem_solving"
+  | "adaptability"
+  | "creativity"
+  | "time_management"
+  | "critical_thinking"
+  | "attention_to_detail"
+  | "empathy"
+  | "resilience";
 
 type BadgeProps = {
   children: React.ReactNode;
@@ -104,6 +130,162 @@ const colorClasses: Record<BadgeColor, string> = {
     text-[#3c873a] 
     border border-zinc-300 dark:border-white/10 
     hover:bg-[#3c873a]/20
+  `,
+  springboot: `
+    bg-[#6db33f]/10
+    text-[#6db33f]
+    border border-zinc-300 dark:border-white/10
+    hover:bg-[#6db33f]/20
+  `,
+  express: `
+    bg-[#000000]/10
+    text-[#000000]
+    border border-zinc-300 dark:border-white/10
+    hover:bg-[#000000]/20
+  `,
+  django: `
+    bg-[#092e20]/10
+    text-[#092e20]
+    border border-zinc-300 dark:border-white/10
+    hover:bg-[#092e20]/20
+  `,
+  MySQL: `
+    bg-[#f49f2f]/10 
+    text-[#f49f2f]
+    border border-zinc-300 dark:border-white/10
+    hover:bg-[#f49f2f]/20
+  `,
+  Docker: `
+    bg-[#1c90ed]/10
+    text-[#1c90ed]
+    border border-zinc-300 dark:border-white/10
+    hover:bg-[#1c90ed]/20
+  `,
+  WampServer: `
+    bg-[#ff0099]/10
+    text-[#ff0099]
+    border border-zinc-300 dark:border-white/10
+    hover:bg-[#ff0099]/20
+  `,
+  Trello: `
+    bg-[#0079bf]/10
+    text-[#0079bf]
+    border border-zinc-300 dark:border-white/10
+    hover:bg-[#0079bf]/20
+  `,
+  python: `
+    bg-[#ffe05d]/10
+    text-[#ffe05d]
+    border border-zinc-300 dark:border-white/10
+    hover:bg-[#ffe05d]/20
+  `,
+  gitlab: `
+    bg-[#e24329]/10
+    text-[#e24329]
+    border border-zinc-300 dark:border-white/10
+    hover:bg-[#e24329]/20
+  `,
+  github: `
+    bg-[#f0f6fc]/10
+    text-[#f0f6fc]
+    border border-zinc-300 dark:border-white/10
+    hover:bg-[#f0f6fc]/20
+  `,
+  postman: `
+    bg-[#ff6c37]/10
+    text-[#ff6c37]
+    border border-zinc-300 dark:border-white/10
+    hover:bg-[#ff6c37]/20
+  `,
+  virtualbox: `
+    bg-[#183a9e]/10
+    text-[#183a9e]
+    border border-zinc-300 dark:border-white/10
+    hover:bg-[#183a9e]/20
+  `,
+  teams: `
+    bg-[#6264a7]/10
+    text-[#6264a7]
+    border border-zinc-300 dark:border-white/10
+    hover:bg-[#6264a7]/20
+  `,
+  microsoft365: `
+    bg-[#0078d4]/10
+    text-[#0078d4]
+    border border-zinc-300 dark:border-white/10
+    hover:bg-[#0078d4]/20
+  `,
+  curious: `
+    bg-zinc-800/10 
+    text-zinc-800 dark:text-white 
+    border border-zinc-300 dark:border-white/10 
+    hover:bg-zinc-800/20 dark:hover:bg-white/10
+  `,
+  leadership: `
+    bg-zinc-800/10 
+    text-zinc-800 dark:text-white 
+    border border-zinc-300 dark:border-white/10 
+    hover:bg-zinc-800/20 dark:hover:bg-white/10
+  `,
+  teamwork: `
+    bg-zinc-800/10 
+    text-zinc-800 dark:text-white 
+    border border-zinc-300 dark:border-white/10 
+    hover:bg-zinc-800/20 dark:hover:bg-white/10
+  `,
+  communication: `
+    bg-zinc-800/10 
+    text-zinc-800 dark:text-white 
+    border border-zinc-300 dark:border-white/10 
+    hover:bg-zinc-800/20 dark:hover:bg-white/10
+  `,
+  problem_solving: `
+    bg-zinc-800/10 
+    text-zinc-800 dark:text-white 
+    border border-zinc-300 dark:border-white/10 
+    hover:bg-zinc-800/20 dark:hover:bg-white/10
+  `,
+  adaptability: `
+    bg-zinc-800/10 
+    text-zinc-800 dark:text-white 
+    border border-zinc-300 dark:border-white/10 
+    hover:bg-zinc-800/20 dark:hover:bg-white/10
+  `,
+  creativity: `
+    bg-zinc-800/10 
+    text-zinc-800 dark:text-white 
+    border border-zinc-300 dark:border-white/10 
+    hover:bg-zinc-800/20 dark:hover:bg-white/10
+  `,
+  time_management: `
+    bg-zinc-800/10 
+    text-zinc-800 dark:text-white 
+    border border-zinc-300 dark:border-white/10 
+    hover:bg-zinc-800/20 dark:hover:bg-white/10
+  `,
+  critical_thinking: `
+    bg-zinc-800/10 
+    text-zinc-800 dark:text-white 
+    border border-zinc-300 dark:border-white/10 
+    hover:bg-zinc-800/20 dark:hover:bg-white/10
+  `,
+  attention_to_detail: `
+    bg-zinc-800/10 
+    text-zinc-800 dark:text-white 
+    border border-zinc-300 dark:border-white/10 
+    hover:bg-zinc-800/20 dark:hover:bg-white/10
+  `,
+  empathy: `
+    bg-zinc-800/10 
+    text-zinc-800 dark:text-white 
+    border border-zinc-300 dark:border-white/10 
+    hover:bg-zinc-800/20 dark:hover:bg-white/10
+  `,
+  resilience: `
+    bg-zinc-800/10 
+    text-zinc-800 dark:text-white 
+    border border-zinc-300 dark:border-white/10 
+    hover:bg-zinc-800/20 dark:hover:bg-white/10
   `,
 };
 
