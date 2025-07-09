71db0801683f815816f73210a93a2fc04dfbf19d
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-07-10 01:21:47 +0200

    Add z-index to Footer component for improved layering

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="relative mt-10 px-6 sm:px-20 py-10 border-t border-zinc-300 dark:border-white/10 text-sm text-zinc-600 dark:text-zinc-400 flex flex-col sm:flex-row justify-between items-center gap-4 overflow-hidden">
+    <footer className="relative mt-10 px-6 sm:px-20 py-10 border-t border-zinc-300 dark:border-white/10 text-sm text-zinc-600 dark:text-zinc-400 flex flex-col sm:flex-row justify-between items-center gap-4 overflow-hidden z-1">
       <p>© {new Date().getFullYear()} Pierre MICHEL. All rights reserved.</p>
       <div className="flex gap-4">
         <Link
