2e7ab8fa9c5b21a3729e0ce5ad837c17c0897032
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-07-10 12:29:46 +0200

    Add experiences and education constants for portfolio

--- src/app/const.tsx
+++ src/app/const.tsx
@@ -14,3 +14,45 @@ export const quickLinks = [
 
 
 {/* About */}
+export const experiences = [
+  {
+    company: "SynapsIA",
+    role: "Fullstack Developer",
+    startDate: "Apr 2025",
+    endDate: "June 2025",
+    description:
+      "Complete development of an internal application for reporting, project management and team management, with customized statistics and a modern environment.",
+    imageSrc: "",
+    linkWork: "/work",
+    technologies: [
+      "php",
+      "html",
+      "css",
+      "js",
+      "mysql",
+      "docker",
+      "wampserver",
+      "trello",
+      "github",
+    ],
+  },
+  {
+    company: "LORIA",
+    role: "Fullstack Developer",
+    startDate: "May 2024",
+    endDate: "July 2024",
+    description:
+      "Development of a Letter of Recommendation management application for LORIA applications",
+    imageSrc: "",
+    technologies: ["python", "html", "css", "js", "sqlite", "docker", "gitlab"],
+  },
+];
+
+export const educations = [
+  {
+    institution: "Metz Numeric School",
+    fieldOfStudy: "Master of Computer Engineering Development",
+    startDate: "2023",
+    endDate: "Present",
+  },
+];
