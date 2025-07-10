86286d98b17c71d8a278ce54aba24bd32d2866aa
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-07-10 12:28:30 +0200

    Add quickLinks constant for navigation in the app

---
+++ src/app/const.tsx
@@ -0,0 +1,16 @@
+{/* App */}
+export const quickLinks = [
+  {
+    title: "About Me",
+    description: "Learn more about who I am and my journey.",
+    href: "/about",
+  },
+  {
+    title: "My Work",
+    description: "Check out projects I've built and contributed to.",
+    href: "/work",
+  },
+];
+
+
+{/* About */}
